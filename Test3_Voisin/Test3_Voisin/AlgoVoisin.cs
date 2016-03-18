77c0a186eca7174295d5b222b186de114790ee96
Miki-chan <marjorie.goudal@devinci.fr>
2016-03-18 15:59:49 +0100

    test

--- Test3_Voisin/Test3_Voisin/AlgoVoisin.cs
+++ Test3_Voisin/Test3_Voisin/AlgoVoisin.cs
@@ -16,7 +16,7 @@ namespace Test3_Voisin
             //int counter = 0;
             string line;
             int i = 0;
-
+            // test
             //faire en sorte que ca marche par tout 
             System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Marjorie\Desktop\PI²\object simple ex\C_002.txt");
             while ((line = file.ReadLine()) != null)
